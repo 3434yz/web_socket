65ba17085ee12cb50f73e714f94b2ce21b7c2655
wenyi <672317451@qq.com>
2024-02-16 17:54:57 +0800

    feat:整合gin框架

--- gin-websocket/server/main.go
+++ gin-websocket/server/main.go
@@ -1,1 +1,55 @@
-package server
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func main() {
+	// 创建Gin应用
+	app := gin.Default()
+
+	// 注册WebSocket路由
+	app.GET("/ws", WebSocketHandler)
+
+	// 启动应用
+	err := app.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func WebSocketHandler(c *gin.Context) {
+	// 获取WebSocket连接
+	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// 处理WebSocket消息
+	for {
+		messageType, p, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+
+		fmt.Println("messageType:", messageType)
+		fmt.Println("p:", string(p))
+
+		// 输出WebSocket消息内容
+		c.Writer.Write(p)
+	}
+
+	// 关闭WebSocket连接
+	ws.Close()
+}
