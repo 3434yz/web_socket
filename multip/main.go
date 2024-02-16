a88c90247651876d699cf0e0b0128aab060ba9c3
wenyi <672317451@qq.com>
2024-02-16 17:54:33 +0800

    doc:拆分函数

--- multip/main.go
+++ multip/main.go
@@ -31,42 +31,66 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "home.html")
 }
 
+func echo(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	roomID := vars["room"]
+
+	mutexForRoomMutexes.Lock()
+	roomMutex, ok := roomMutexes[roomID]
+	if ok {
+		roomMutex.Lock()
+	} else {
+		roomMutexes[roomID] = new(sync.Mutex)
+		roomMutexes[roomID].Lock()
+	}
+	mutexForRoomMutexes.Unlock()
+
+	//room, ok := house[roomID]
+	room, ok := house.Load(roomID)
+	//fmt.Println("Sleep 10 seconds")
+	//time.Sleep(time.Second * 10)
+
+	var hub *Hub
+	if ok {
+		fmt.Println("Found room")
+		hub = room.(*Hub)
+	} else {
+		fmt.Println("Create room")
+		hub = newHub(roomID)
+		//house[roomID] = hub
+		house.Store(roomID, hub)
+		go hub.run()
+	}
+	serveWs(hub, writer, request)
+}
+
+type Auth struct {
+	Next http.Handler
+}
+
+func NewAuth(handler http.Handler) *Auth {
+	return &Auth{Next: handler}
+}
+
+func (a *Auth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if a.Next == nil {
+		a.Next = http.DefaultServeMux
+	}
+	//auth := r.Header.Get("Authen")
+	//if auth == "" {
+	//	w.WriteHeader(http.StatusUnauthorized)
+	//} else {
+	//	a.Next.ServeHTTP(w, r)
+	//}
+	fmt.Println("------------------------------------------------------")
+}
+
 func main() {
 	flag.Parse()
 	r := mux.NewRouter()
 	r.HandleFunc("/{room}", serveHome)
-	r.HandleFunc("/ws/{room}", func(writer http.ResponseWriter, request *http.Request) {
-		vars := mux.Vars(request)
-		roomID := vars["room"]
-
-		mutexForRoomMutexes.Lock()
-		roomMutex, ok := roomMutexes[roomID]
-		if ok {
-			roomMutex.Lock()
-		} else {
-			roomMutexes[roomID] = new(sync.Mutex)
-			roomMutexes[roomID].Lock()
-		}
-		mutexForRoomMutexes.Unlock()
-
-		//room, ok := house[roomID]
-		room, ok := house.Load(roomID)
-		//fmt.Println("Sleep 10 seconds")
-		//time.Sleep(time.Second * 10)
-
-		var hub *Hub
-		if ok {
-			fmt.Println("Found room")
-			hub = room.(*Hub)
-		} else {
-			fmt.Println("Create room")
-			hub = newHub(roomID)
-			//house[roomID] = hub
-			house.Store(roomID, hub)
-			go hub.run()
-		}
-		serveWs(hub, writer, request)
-	})
+	r.HandleFunc("/ws/{room}", echo)
+	r.Handle("/ws/{room}", NewAuth(r))
 	server := &http.Server{
 		Addr:              *addr,
 		ReadHeaderTimeout: 3 * time.Second,
